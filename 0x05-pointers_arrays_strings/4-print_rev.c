d233be1fd80f0657efa716582be62a6d12de631e
root <odonkorcedrickwabena@gmail.com>
2023-07-24 17:22:26 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * Return: The length of the string (excluding the null terminator)
+*/
+int _strlen(char *s)
 {
-	return (write(1, &c, 1));
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
 }
