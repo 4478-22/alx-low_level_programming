4922cd0d95923e0694c02bf6d6fd65b9d45e5df4
4478-22 <odonkorcedrickwabena@gmail.com>
2023-10-02 16:52:14 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,18 +13,18 @@ void close_file(int fd);
  */
 char *create_buffer(char *file)
 {
-	char *buf;
+	char *buffer;
 
-	buf = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (buf == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	return (buf);
+	return (buffer);
 }
 
 /**
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 		tolp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || rio == -1)
+		if (fiyr == -1 || rio == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
